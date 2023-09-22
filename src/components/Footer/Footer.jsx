8150639dff3a374f40a9fc0b94ac79f2d1c134a9
MateuszBachor <mateuszbachor3@gmail.com>
2023-09-22 11:54:17 +0200

    new file:   build.zip 	modified:   src/App.css 	modified:   src/App.js 	new file:   src/components/Footer/ContactForm.jsx 	modified:   src/components/Footer/Footer.jsx 	modified:   src/components/Footer/Footer.module.css 	new file:   src/components/Footer/whyus.jpg 	deleted:    src/components/MapComponent/1.jpg 	deleted:    src/components/MapComponent/2.jpg 	deleted:    src/components/MapComponent/3jpg.jpg 	deleted:    src/components/MapComponent/4.jpg 	modified:   src/components/OurOffer/OurOffer.jsx 	modified:   src/components/OurOffer/OurOffer.module.css 	new file:   src/components/OurOffer/SerwisImg.png 	new file:   src/components/WhyUs/WhyUs.jsx 	new file:   src/components/WhyUs/WhyUs.module.css new file:   src/components/WhyUs2/WhyUs2.jsx 	new file:   src/components/WhyUs2/WhyUs2.module.css 	new file:   src/components/WhyUs2/whyus.jpg 	modified:   src/index.css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,52 +3,12 @@ import styles from "./Footer.module.css";
 import { FiMapPin } from "react-icons/fi";
 import { BsTelephone } from "react-icons/bs";
 import { HiOutlineMail } from "react-icons/hi";
-import MyImage from "./EliteImg.png";
 import Hotline from "./Hotline.png";
+import MyImage from "./EliteImg.png";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.contactContainer}>
-        <div className={styles.contact}>
-          <h4>Kontakt</h4>
-          <span>
-            <div className={styles.contactIcon}>
-              <BsTelephone />
-            </div>
-            888-123-456
-          </span>
-          <span>
-            <div className={styles.contactIcon}>
-              <HiOutlineMail />
-            </div>
-            kontakt@888technicalsecurity.com
-          </span>
-          <span>
-            <div className={styles.contactIcon}>
-              <FiMapPin />
-            </div>
-            Strzelin Jana Pawła II 12/1
-          </span>
-          <span style={{ height: "50px", marginTop: "40px", fontSize: "19px" }}>
-            <img src={Hotline} alt="" className={styles.hotlineImg} />
-            71 234 234 12
-          </span>
-        </div>
-        <div className={styles.contactForm}>
-          <h4 style={{ textAlign: "left", marginLeft: "25px" }}>
-            Formularz kontaktowy
-          </h4>
-          <form>
-            <input type="email" placeholder="Email" />
-            <input type="text" placeholder="Imię i nazwisko" />
-            <textarea placeholder="Wiadomość"></textarea>
-            <span>
-              <button type="submit">Wyślij</button>
-            </span>
-          </form>
-        </div>
-      </div>
       <div className={styles.info}>
         <div className={styles.infoFlex}>
           <h4 className={styles.infoTitle}>O Firmie</h4>
--- src/components/OurOffer/OurOffer.jsx
+++ src/components/OurOffer/OurOffer.jsx
@@ -1,45 +1,86 @@
 import React, { useState, useEffect } from "react";
 import styles from "./OurOffer.module.css";
+import img from "./SerwisImg.png";
 
 const OurOffer = () => {
   const offers = [
     {
       title: "Bezpieczeństwo fizyczne",
       description:
-        "-Zapewniamy 1\n-proewnić ci 2\n-spokój i reagować \n-na wszelkie zagrożenia.",
-      icon: "https://nawakara.com/wp-content/uploads/2022/02/1643951013485.jpg",
+        "- Montaż oraz serwis systemów alarmowych i napadowych\n- Montaż oraz serwis systemów telewizji przemysłowej CCTV i  wideomonitoringu\n- Montaż oraz serwis instalacji domofonowych i wideodomofonów\n- Pomoc techniczna i  konserwacje\n- Usługi elektryczne",
+      backgroundImage: img,
     },
     {
       title: "Ochrona danych",
       description:
-        "-Zapewniamy 1\n-proewnić ci 2\n-spokój i reagować \n-na wszelkie zagrożenia.",
-      icon: "https://www.simplilearn.com/ice9/free_resources_article_thumb/Top_Cybersecurity_Projects.jpg",
+        "- Montaż oraz serwis systemów alarmowych i napadowych\n- Montaż oraz serwis systemów telewizji przemysłowej CCTV i  wideomonitoringu\n- Montaż oraz serwis instalacji domofonowych i wideodomofonów\n- Pomoc techniczna i  konserwacje\n- Usługi elektryczne",
+      backgroundImage: img,
     },
     {
       title: "Monitoring 24/7",
+
       description:
-        "-Zapewniamy 1\n-proewnić ci 2\n-spokój i reagować \n-na wszelkie zagrożenia.",
-      icon: "https://best-security.pl/wp-content/uploads/2021/11/monitoring.jpg",
+        "- Montaż oraz serwis systemów alarmowych i napadowych\n- Montaż oraz serwis systemów telewizji przemysłowej CCTV i  wideomonitoringu\n- Montaż oraz serwis instalacji domofonowych i wideodomofonów\n- Pomoc techniczna i  konserwacje\n- Usługi elektryczne",
+      backgroundImage: img,
     },
     {
       title: "Bezpieczeństwo fizyczne",
       description:
-        "-Zapewniamy 1\n-proewnić ci 2\n-spokój i reagować \n-na wszelkie zagrożenia.",
-      icon: "https://nawakara.com/wp-content/uploads/2022/02/1643951013485.jpg",
+        "- Montaż oraz serwis systemów alarmowych i napadowych\n- Montaż oraz serwis systemów telewizji przemysłowej CCTV i  wideomonitoringu\n- Montaż oraz serwis instalacji domofonowych i wideodomofonów\n- Pomoc techniczna i  konserwacje\n- Usługi elektryczne",
+      backgroundImage: img,
     },
     {
       title: "Sprzątnie",
       description:
-        "-Zapewniamy 1\n-proewnić ci 2\n-spokój i reagować \n-na wszelkie zagrożenia.",
-      icon: "https://oto-mistrz.pl/wp-content/uploads/2022/07/cs_blog_header.jpg",
+        "- Montaż oraz serwis systemów alarmowych i napadowych\n- Montaż oraz serwis systemów telewizji przemysłowej CCTV i  wideomonitoringu\n- Montaż oraz serwis instalacji domofonowych i wideodomofonów\n- Pomoc techniczna i  konserwacje\n- Usługi elektryczne",
+      backgroundImage: img,
     },
     {
       title: "Serwis",
       description:
-        "-Zapewniamy 1\n-proewnić ci 2\n-spokój i reagować \n-na wszelkie zagrożenia.",
-      icon: "https://elektrykdlakazdego.pl/wp-content/uploads/2022/07/bigstock-A-Male-Electrician-Works-In-A-252302767-pavjel-500x352.jpg",
+        "- Montaż oraz serwis systemów alarmowych i napadowych\n- Montaż oraz serwis systemów CCTV i wideomonitoringu\n- Montaż oraz serwis instalacji domofonowych i wideodomofonów\n- Pomoc techniczna i  konserwacje\n- Usługi elektryczne",
+      backgroundImage: img,
     },
   ];
+  // const offers = [
+  //   {
+  //     title: "Bezpieczeństwo fizyczne",
+  //     description:
+  //       "- Montaż oraz serwis systemów alarmowych i napadowych\n- Montaż oraz serwis systemów telewizji przemysłowej CCTV i  wideomonitoringu\n- Montaż oraz serwis instalacji domofonowych i wideodomofonów\n- Pomoc techniczna i  konserwacje\n- Usługi elektryczne",
+  //     backgroundImage: img,
+  //   },
+  //   {
+  //     title: "Ochrona danych",
+  //     description:
+  //       "- Montaż oraz serwis systemów alarmowych i napadowych\n- Montaż oraz serwis systemów telewizji przemysłowej CCTV i  wideomonitoringu\n- Montaż oraz serwis instalacji domofonowych i wideodomofonów\n- Pomoc techniczna i  konserwacje\n- Usługi elektryczne",
+  //     backgroundImage: "https://www.simplilearn.com/ice9/free_resources_article_thumb/Top_Cybersecurity_Projects.jpg",
+  //   },
+  //   {
+  //     title: "Monitoring 24/7",
+
+  //     description:
+  //       "- Montaż oraz serwis systemów alarmowych i napadowych\n- Montaż oraz serwis systemów telewizji przemysłowej CCTV i  wideomonitoringu\n- Montaż oraz serwis instalacji domofonowych i wideodomofonów\n- Pomoc techniczna i  konserwacje\n- Usługi elektryczne",
+  //     backgroundImage: "https://best-security.pl/wp-content/uploads/2021/11/monitoring.jpg",
+  //   },
+  //   {
+  //     title: "Bezpieczeństwo fizyczne",
+  //     description:
+  //       "- Montaż oraz serwis systemów alarmowych i napadowych\n- Montaż oraz serwis systemów telewizji przemysłowej CCTV i  wideomonitoringu\n- Montaż oraz serwis instalacji domofonowych i wideodomofonów\n- Pomoc techniczna i  konserwacje\n- Usługi elektryczne",
+  //     backgroundImage: "https://nawakara.com/wp-content/uploads/2022/02/1643951013485.jpg",
+  //   },
+  //   {
+  //     title: "Sprzątnie",
+  //     description:
+  //       "- Montaż oraz serwis systemów alarmowych i napadowych\n- Montaż oraz serwis systemów telewizji przemysłowej CCTV i  wideomonitoringu\n- Montaż oraz serwis instalacji domofonowych i wideodomofonów\n- Pomoc techniczna i  konserwacje\n- Usługi elektryczne",
+  //     backgroundImage: "https://oto-mistrz.pl/wp-content/uploads/2022/07/cs_blog_header.jpg",
+  //   },
+  //   {
+  //     title: "Serwis",
+  //     description:
+  //       "- Montaż oraz serwis systemów alarmowych i napadowych\n- Montaż oraz serwis systemów CCTV i wideomonitoringu\n- Montaż oraz serwis instalacji domofonowych i wideodomofonów\n- Pomoc techniczna i  konserwacje\n- Usługi elektryczne",
+  //     backgroundImage: "https://elektrykdlakazdego.pl/wp-content/uploads/2022/07/bigstock-A-Male-Electrician-Works-In-A-252302767-pavjel-500x352.jpg",
+  //   },
+  // ];
   const [isCentered, setIsCentered] = useState(false);
   useEffect(() => {
     const handleScroll = () => {
@@ -62,31 +103,36 @@ const OurOffer = () => {
       <h2>Sprawdź naszą oferte!</h2>
       <div className={styles.offerCards}>
         {offers.map((offer, index) => (
-          <div
-            className={styles.offerCard}
-            key={index}
-            style={{
-              backgroundImage: `url(${offer.icon})`,
-            }}
-          >
-            <div className={styles.descriptionContainer} style={{}}>
-              <div
-                className={`${styles.description} ${
-                  isCentered ? styles.centred : ""
-                }`}
-              >
-                {offer.description.split("\n").map((line, index) => (
-                  <p key={index} className={styles.text}>
-                    {line}
-                  </p>
-                ))}
-                <div className={styles.readMore}>Czytaj więcej...</div>
-              </div>
-            </div>
-            <div className={styles.offerTitle}>
-              <h3>{offer.title}</h3>
-            </div>
+          <div className={styles.offerCard} key={index}>
+            <img src={offer.backgroundImage} alt="" />
           </div>
+          // <div
+          //   className={styles.offerCard}
+          //   key={index}
+          //   style={{
+          //     backgroundImage: `url(${offer.backgroundImage})`,
+          //   }}
+          // >
+          //   <div className={styles.offerTitle}>
+          //     {/* <span>{offer.title}</span> */}
+          //   </div>
+          //   <div className={styles.descriptionContainer}>
+          //     <div
+          //       className={`${styles.description} ${
+          //         isCentered ? styles.centred : ""
+          //       }`}
+          //     >
+          //       {offer.description.split("\n").map((line, index) => (
+          //         <p key={index} className={styles.text}>
+          //           {line}
+          //         </p>
+          //       ))}
+          //     </div>
+          //   </div>
+          //   <div className={styles.readMore}>
+          //     <button>Zobacz więcej...</button>
+          //   </div>
+          // </div>
         ))}
       </div>
     </section>
